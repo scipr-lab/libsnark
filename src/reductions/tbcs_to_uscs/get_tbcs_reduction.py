446eb80296f180743a8451dcb4804d59c380a32b
Madars Virza <madars@mit.edu>
2015-04-23 16:22:01 -0400

    Specify license for the TBCS to USCS reduction script.

--- src/reductions/tbcs_to_uscs/get_tbcs_reduction.py
+++ src/reductions/tbcs_to_uscs/get_tbcs_reduction.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+##
+# @author     This file is part of libsnark, developed by SCIPR Lab
+#             and contributors (see AUTHORS).
+# @copyright  MIT license (see LICENSE file)
+
 from __future__ import division
 import itertools
 
