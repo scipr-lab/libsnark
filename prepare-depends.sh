006b118255019c8d6b48a065c719c1d217b22458
mbb <mbb@di.uminho.pt>
2015-08-20 11:54:02 +0100

    adsnark extension

--- prepare-depends.sh
+++ prepare-depends.sh
@@ -13,9 +13,16 @@ mkdir -p $DEPSRC
 cd $DEPSRC
 [ ! -d xbyak ] && git clone git://github.com/herumi/xbyak.git
 [ ! -d ate-pairing ] && git clone git://github.com/herumi/ate-pairing.git
+[ ! -d libsnark-supercop ] && git clone git://github.com/mbbarbosa/libsnark-supercop.git
 cd ate-pairing
 make -j SUPPORT_SNARK=1
+cd ..
+cd libsnark-supercop
+sh "do"
 cd ..
 cd ..
 cp -rv $DEPSRC/ate-pairing/include $DEPINST/
 cp -rv $DEPSRC/ate-pairing/lib $DEPINST/
+mkdir -p $DEPINST/include/supercop
+cp -v $DEPSRC/libsnark-supercop/include/* $DEPINST/include/supercop
+cp -rv $DEPSRC/libsnark-supercop/lib $DEPINST/
